bbdccdec580e2bbf8cf676140ac07d04098a8f53
chrbcho <chrbcho@gmail.com>
2026-01-09 11:28:09 -0800

    feat: define text_lower() stub

---
+++ src/text_processor/text_lower.py
@@ -0,0 +1,25 @@
+"""
+A module that reads in a text file, converts all characters to lowercase, and then writes the output to a new file.
+"""
+
+def text_lower(input_filepath, output_filepath):
+    """
+    Converts all characters in a text file to lowercase, then writes the result into another file.
+    
+    Parameters
+    ----------
+    input_filepath: str
+        Path to the source text file to be converted
+    output_filepath: str
+        Path where the converted text tile will be saved
+
+    Returns
+    -------
+    None
+        Function writes directly to a file
+
+    Examples
+    --------
+    >>> text_lower("example.txt", "example_lower.txt")
+    """
+    pass
