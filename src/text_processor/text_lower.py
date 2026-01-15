796e2f40e2e60329eff3c77c1400486396380a87
chrbcho <chrbcho@gmail.com>
2026-01-14 21:56:20 -0800

    feat: update text_lower() docstring

--- src/text_processor/text_lower.py
+++ src/text_processor/text_lower.py
@@ -1,5 +1,5 @@
 """
-A module that reads a text file, converts all characters to lowercase, then writes the output to a new file.
+A module that reads a UTF-8 text file, converts all uppercase characters to lowercase, then writes the output to a new file.
 """
 
 def text_lower(input_path: str, output_path: str):
@@ -9,14 +9,25 @@ def text_lower(input_path: str, output_path: str):
     Parameters
     ----------
     input_path : str
-        Path to the input text file.
+        Path to the input text file. Must be a string ending with .txt
     output_path : str
-        Path to write the processed text file after removal.
+        Path to write the processed text file. Must be a string ending with .txt. If input and output paths are the same, the input file will be overwritten.
 
     Returns
     -------
     None
-        Function writes directly to a file
+        Function writes directly to a UTF-8 file and returns None.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the input file does not exist.
+    TypeError
+        If input_path or output_path is not a string.
+    ValueError
+        If input_path or output_path does not have a .txt file extension.
+    OSError
+        If there is an error reading from or writing to the file.
 
     Examples
     --------
